37038d9322968099003af87ab48cae2fda00993e
tiago <tiagoaosilveira@gmail.com>
2024-06-21 09:27:23 -0300

    added multiple tries

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import { useCallback, useEffect, useState } from 'react'
+import {useCallback, useEffect, useMemo, useState} from 'react'
 import './App.css'
+
+const wordList = [
+  'BAKE',
+  'FOUR',
+  'GOOD',
+  'ZERO',
+  'TREE',
+  'HOPE',
+  'HUGE',
+  'FIVE',
+  'RACE',
+  'MARS',
+  'STAY',
+  'CAME'
+];
 
 function App() {
   const wordSize = 4;
-  const wordList = [
-    'BAKE',
-    'FOUR',
-    'GOOD',
-    'ZERO',
-    'TREE',
-    'HOPE',
-    'HUGE',
-    'FIVE',
-    'RACE',
-    'MARS',
-    'STAY',
-    'CAME'
-  ];
+  const solution = useMemo(() => wordList[Math.floor(Math.random() * wordList.length)], []);
 
-  const [solution, setSolution] = useState(wordList[Math.floor(Math.random() * wordList.length)]);
   const [currentTry, setCurrentTry] = useState(1);
   const [currentPos, setCurrentPos] = useState(0);
+  const [result, setResult] = useState('');
   const [error, setError] = useState('');
   const [tries, setTries] = useState(new Map([
     ['try1', Array(4).fill(['',''])],
@@ -31,16 +33,31 @@ function App() {
   const solve = useCallback(() => {
     for (let i = 0; i < wordSize; i++) {
       for (let j = 0; j < wordSize; j++) {
-        if (tries.get(`try${currentTry}`)[i][0] === solution[i]) {
-          setTries(prevState => {
-            const newState = new Map(prevState);
-            const updatedTryData = newState.get(`try${currentTry}`);
-            updatedTryData[i][1] = 'green-square';
-            return newState.set(`try${currentTry}`, updatedTryData);
-          })
+        if (tries.get(`try${currentTry}`)[i][0] === solution[j]) {
+          if (i === j) {
+            setTries(prevState => {
+              const newState = new Map(prevState);
+              const updatedTryData = newState.get(`try${currentTry}`);
+              updatedTryData[i][1] = 'green-square';
+              return newState.set(`try${currentTry}`, updatedTryData);
+            });
+            break;
+          } else {
+            setTries(prevState => {
+              const newState = new Map(prevState);
+              const updatedTryData = newState.get(`try${currentTry}`);
+              updatedTryData[i][1] = 'yellow-square';
+              return newState.set(`try${currentTry}`, updatedTryData);
+            });
+          }
         }
       }
     }
+    if (tries.get(`try${currentTry}`).every(curr => curr === 'green-square')) {
+      return setResult('WIN');
+    }
+    setCurrentTry(currentTry + 1);
+    setCurrentPos(0);
   }, [currentTry, solution, tries]);
 
   useEffect(() => {
@@ -84,11 +101,22 @@ function App() {
   return (
     <>
       <div className='try-container'>
-        { tries.get('try1').map(((try1, idx) => {
+        {tries.get('try1').map(((try1, idx) => {
           return <div className={`box ${try1[1]}`} key={idx}>{try1[0]}</div>
         }))}
       </div>
-      { error && <span>{error}</span> }
+      <div className='try-container'>
+        {tries.get('try2').map(((try2, idx) => {
+          return <div className={`box ${try2[1]}`} key={idx}>{try2[0]}</div>
+        }))}
+      </div>
+      <div className='try-container'>
+        {tries.get('try3').map(((try3, idx) => {
+          return <div className={`box ${try3[1]}`} key={idx}>{try3[0]}</div>
+        }))}
+      </div>
+      {solution}
+      {error && <span>{error}</span>}
     </>
   )
 }
